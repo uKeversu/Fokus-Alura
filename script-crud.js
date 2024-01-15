3aed49bade7e537e546a574302a62924c10f5359
uKeversu <109996005+uKeversu@users.noreply.github.com>
2024-01-15 19:08:37 -0300

    Aula 5 - Tarefas concluídas
    Remover tarefas concluídas.

--- script-crud.js
+++ script-crud.js
@@ -9,7 +9,9 @@ const paragrafoDescricaoTarefa = document.querySelector(
   ".app__section-active-task-description"
 );
 
-const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+const btnRemoverConcluidas = document.querySelector("#btn-remover-concluidas");
+
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 let tarefaSelecionada = null;
 let liTarefaSelecionada = null;
 
@@ -55,25 +57,29 @@ function criarElementoTarefa(tarefa) {
   li.append(paragrafo);
   li.append(botao);
 
-  li.onclick = () => {
-    document
-      .querySelectorAll(".app__section-task-list-item-active")
-      .forEach((elemento) => {
-        elemento.classList.remove("app__section-task-list-item-active");
-      });
-    if (tarefaSelecionada == tarefa) {
-      paragrafoDescricaoTarefa.textContent = "";
-      tarefaSelecionada = null;
-      liTarefaSelecionada = null;
-      return;
-    }
-    liTarefaSelecionada = li;
-    tarefaSelecionada = tarefa;
-    paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+  if (tarefa.completa) {
+    li.classList.add("app__section-task-list-item-complete");
+    botao.setAttribute("disabled", "disabled");
+  } else {
+    li.onclick = () => {
+      document
+        .querySelectorAll(".app__section-task-list-item-active")
+        .forEach((elemento) => {
+          elemento.classList.remove("app__section-task-list-item-active");
+        });
+      if (tarefaSelecionada == tarefa) {
+        paragrafoDescricaoTarefa.textContent = "";
+        tarefaSelecionada = null;
+        liTarefaSelecionada = null;
+        return;
+      }
+      liTarefaSelecionada = li;
+      tarefaSelecionada = tarefa;
+      paragrafoDescricaoTarefa.textContent = tarefa.descricao;
 
-    li.classList.add("app__section-task-list-item-active");
-  };
-
+      li.classList.add("app__section-task-list-item-active");
+    };
+  }
   return li;
 }
 
@@ -111,5 +117,16 @@ document.addEventListener("FocoFinalizado", () => {
     liTarefaSelecionada
       .querySelector("button")
       .setAttribute("disabled", "disabled");
+    tarefaSelecionada.completa = true;
+    atualizarTarefas();
   }
 });
+
+btnRemoverConcluidas.onclick = () => {
+  const seletor = ".app__section-task-list-item-complete";
+  document.querySelectorAll(seletor).forEach((elemento) => {
+    elemento.remove();
+  });
+  tarefas = tarefas.filter((tarefa) => !tarefa.completa);
+  atualizarTarefas();
+};
